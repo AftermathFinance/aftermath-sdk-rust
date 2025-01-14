481e5c02a0a80fae1d97199e1ba2ad20cd5e5a13
UnMaykr <98741738+unmaykr-aftermath@users.noreply.github.com>
2025-01-14 16:07:53 -0300

    Revert "feat(sui-gql-client): deprecate `ObjectFilter::object_keys`"
    This reverts commit f43324ff8175f8f9007672d73f39761b5ab770b4.

--- crates/sui-gql-client/src/queries/full_objects.rs
+++ crates/sui-gql-client/src/queries/full_objects.rs
@@ -27,19 +27,14 @@ pub(super) async fn query<C: GraphQlClient>(
             })
         });
 
-    #[expect(
-        deprecated,
-        reason = "TODO: build query from scratch with new ObjectFilter"
-    )]
-    let filter = ObjectFilter {
-        object_ids: Some(object_ids),
-        object_keys: Some(object_keys),
-        ..Default::default()
-    };
     let vars = Variables {
         after: None,
         first: page_size.map(|v| v.try_into().unwrap_or(i32::MAX)),
-        filter: Some(filter),
+        filter: Some(ObjectFilter {
+            object_ids: Some(object_ids),
+            object_keys: Some(object_keys),
+            ..Default::default()
+        }),
     };
 
     let (init, pages) = client
