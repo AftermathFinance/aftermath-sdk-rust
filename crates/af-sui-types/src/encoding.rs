5cc98838cd8a8b8223edee8f7944b40ccfeec527
unmaykr (aftermath) <98741738+unmaykr-aftermath@users.noreply.github.com>
2025-04-14 18:07:23 +0400

    refactor(af-sui-types)!: decouple from `base64`
    Also, use `base64ct` internally to power the `encoding` module

--- crates/af-sui-types/src/encoding.rs
+++ crates/af-sui-types/src/encoding.rs
@@ -1,27 +1,19 @@
 //! Encoding types used by Sui.
-//!
-//! This module is mostly to avoid importing [`fastcrypto`](https://github.com/MystenLabs/fastcrypto)
 
-use base64::Engine;
-use base64::engine::GeneralPurpose;
+use base64ct::{Base64, Encoding as _};
 use serde::{Deserialize, Deserializer, Serialize, Serializer};
-use serde_with::base64::Base64;
-use serde_with::{DeserializeAs, SerializeAs, formats};
-
-/// Default encoder for Base64 data.
-pub(crate) const BASE64_DEFAULT_ENGINE: GeneralPurpose = GeneralPurpose::new(
-    &base64::alphabet::STANDARD,
-    base64::engine::general_purpose::PAD,
-);
+use serde_with::{DeserializeAs, SerializeAs};
 
 /// Convenience method for decoding base64 bytes the way Sui expects.
-pub fn decode_base64_default(value: impl AsRef<[u8]>) -> Result<Vec<u8>, base64::DecodeError> {
-    BASE64_DEFAULT_ENGINE.decode(value)
+pub fn decode_base64_default(value: impl AsRef<[u8]>) -> Result<Vec<u8>, Error> {
+    let s = std::str::from_utf8(value.as_ref())
+        .map_err(|e| Error::Base64(format!("Invalid UTF-8: {e}")))?;
+    Base64::decode_vec(s).map_err(|e| Error::Base64(e.to_string()))
 }
 
 /// Convenience method for encoding bytes to base64 the way Sui expects.
 pub fn encode_base64_default(value: impl AsRef<[u8]>) -> String {
-    BASE64_DEFAULT_ENGINE.encode(value)
+    base64ct::Base64::encode_string(value.as_ref())
 }
 
 // =============================================================================
@@ -32,31 +24,24 @@ pub fn encode_base64_default(value: impl AsRef<[u8]>) -> String {
 ///
 /// The type serializes a value as a base64 string of its BCS encoding.
 /// It works on any type compatible with [`bcs`] for (de)serialization.
-pub struct Base64Bcs<Alphabet = serde_with::base64::Standard, Padding = formats::Padded>(
-    std::marker::PhantomData<(Alphabet, Padding)>,
-)
-where
-    Alphabet: serde_with::base64::Alphabet,
-    Padding: formats::Format;
+pub struct Base64Bcs;
 
-impl<'de, T, Alphabet, Padding> DeserializeAs<'de, T> for Base64Bcs<Alphabet, Padding>
+impl<'de, T> DeserializeAs<'de, T> for Base64Bcs
 where
-    Alphabet: serde_with::base64::Alphabet,
-    Padding: formats::Format,
     T: for<'a> Deserialize<'a>,
 {
     fn deserialize_as<D>(deserializer: D) -> Result<T, D::Error>
     where
         D: Deserializer<'de>,
     {
-        let bytes: Vec<u8> = Base64::<Alphabet, Padding>::deserialize_as(deserializer)?;
+        let bytes: Vec<u8> = Base64::decode_vec(&String::deserialize(deserializer)?)
+            .map_err(serde::de::Error::custom)?;
         bcs::from_bytes(&bytes).map_err(serde::de::Error::custom)
     }
 }
 
-impl<T, Alphabet> SerializeAs<T> for Base64Bcs<Alphabet, formats::Padded>
+impl<T> SerializeAs<T> for Base64Bcs
 where
-    Alphabet: serde_with::base64::Alphabet,
     T: Serialize,
 {
     fn serialize_as<S>(source: &T, serializer: S) -> Result<S::Ok, S::Error>
@@ -64,21 +49,7 @@ where
         S: Serializer,
     {
         let bytes = bcs::to_bytes(source).map_err(serde::ser::Error::custom)?;
-        Base64::<Alphabet, formats::Padded>::serialize_as(&bytes, serializer)
-    }
-}
-
-impl<T, Alphabet> SerializeAs<T> for Base64Bcs<Alphabet, formats::Unpadded>
-where
-    Alphabet: serde_with::base64::Alphabet,
-    T: Serialize,
-{
-    fn serialize_as<S>(source: &T, serializer: S) -> Result<S::Ok, S::Error>
-    where
-        S: Serializer,
-    {
-        let bytes = bcs::to_bytes(source).map_err(serde::ser::Error::custom)?;
-        Base64::<Alphabet, formats::Unpadded>::serialize_as(&bytes, serializer)
+        Base64::encode_string(&bytes).serialize(serializer)
     }
 }
 
@@ -90,8 +61,10 @@ where
 pub struct Base58;
 
 impl Base58 {
-    pub fn decode(data: impl AsRef<[u8]>) -> Result<Vec<u8>, bs58::decode::Error> {
-        bs58::decode(data).into_vec()
+    pub fn decode(data: impl AsRef<[u8]>) -> Result<Vec<u8>, Error> {
+        bs58::decode(data)
+            .into_vec()
+            .map_err(|e| Error::Base58(e.to_string()))
     }
 
     pub fn encode(data: impl AsRef<[u8]>) -> String {
@@ -119,7 +92,7 @@ where
 
 #[derive(thiserror::Error, Debug)]
 #[error("Converting from a Byte Vector of length {length}: {error:?}")]
-pub struct BytesConversionError<E> {
+pub(crate) struct BytesConversionError<E> {
     pub length: usize,
     pub error: E,
 }
@@ -134,4 +107,13 @@ where
     {
         Self::encode(value).serialize(serializer)
     }
+}
+
+#[derive(thiserror::Error, Debug)]
+#[non_exhaustive]
+pub enum Error {
+    #[error("Base64: {0}")]
+    Base64(String),
+    #[error("Base58: {0}")]
+    Base58(String),
 }
