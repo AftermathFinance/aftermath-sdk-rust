35bf18a22c27211e9a7ab13b0d496400ca8c5745
matteo-aftermath <matteo@aftermath.finance>
2025-10-09 01:54:00 +0200

    feat(af-pyth-wrapper)!: new gql client
    Signed-off-by: matteo-aftermath <matteo@aftermath.finance>

--- crates/af-pyth-wrapper/src/graphql.rs
+++ crates/af-pyth-wrapper/src/graphql.rs
@@ -2,6 +2,7 @@ use af_move_type::MoveInstance;
 use af_sui_types::Address;
 use sui_framework_sdk::object::ID;
 use sui_gql_client::GraphQlClient;
+use sui_gql_client::queries::model::outputs::DynamicField;
 use sui_gql_client::queries::{Error as QueryError, GraphQlClientExt as _};
 
 type Key = crate::wrapper::PythPriceInfoId;
@@ -10,34 +11,35 @@ type Key = crate::wrapper::PythPriceInfoId;
 #[derive(thiserror::Error, Debug)]
 pub enum Error<C: std::error::Error> {
     #[error("Querying Owner DF content: {0}")]
-    OwnerDfContent(QueryError<C>),
+    ObjectDfQuery(QueryError<C>),
 
     #[error("BCS De/Ser: {0}")]
     Bcs(#[from] sui_sdk_types::bcs::Error),
 
     #[error(transparent)]
     FromRawType(#[from] af_move_type::FromRawTypeError),
+
+    #[error("Found a struct object, expecting Address")]
+    RawInvalidType,
 }
 
-#[trait_variant::make(Send)]
-pub trait GraphQlClientExt: GraphQlClient + Sized {
-    /// Get the ID of the Pyth `PriceInfoObject` for a `PriceFeed`.
-    async fn price_info_object_id_for_feed(
-        &self,
-        pyth_wrapper_pkg: Address,
-        price_feed: Address,
-    ) -> Result<Address, Error<Self::Error>> {
-        async move {
-            let key = Key::new().move_instance(pyth_wrapper_pkg);
-            let raw = self
-                .owner_df_content(price_feed, key.try_into()?, None)
-                .await
-                .map_err(Error::OwnerDfContent)?;
-            let pf: MoveInstance<ID> = raw.try_into()?;
+pub async fn query<C>(
+    client: &C,
+    pyth_wrapper_pkg: Address,
+    price_feed: Address,
+) -> Result<Address, Error<C::Error>>
+where
+    C: GraphQlClient,
+{
+    let key = Key::new().move_instance(pyth_wrapper_pkg);
+    let raw_move_value = client
+        .object_df_by_name(price_feed, key.try_into()?, None)
+        .await;
+    let move_id: MoveInstance<ID> = match raw_move_value {
+        Ok(DynamicField::Field(raw)) => raw.try_into()?,
+        Ok(DynamicField::Object(_, _)) => return Err(Error::RawInvalidType),
+        Err(err) => return Err(Error::ObjectDfQuery(err)),
+    };
 
-            Ok(pf.value.bytes)
-        }
-    }
+    Ok(move_id.value.bytes)
 }
-
-impl<T: GraphQlClient> GraphQlClientExt for T {}
