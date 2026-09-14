103edc1a7c67e49a0e0f0bab90c6dc334ec3dc2b
Nikolai <nikolai@aftermath.finance>
2026-09-14 13:26:13 +0200

    feat!: update dependencies, fix clippy warnings

--- crates/af-iperps/examples/ptb/cancel_orders.rs
+++ crates/af-iperps/examples/ptb/cancel_orders.rs
@@ -51,7 +51,7 @@ async fn main() -> Result<()> {
     let ptb = ptb!(
         package perpetuals: perpetuals_package;
 
-        type T = otw.into();
+        type T = otw;
 
         input obj clearing_house: clearing_house;
         input obj account: account;
--- crates/sui-jsonrpc/src/client.rs
+++ crates/sui-jsonrpc/src/client.rs
@@ -512,6 +512,10 @@ impl SuiClient {
     /// Estimate a budget for the transaction by dry-running it.
     ///
     /// Uses default [`GasBudgetOptions`] to compute the cost estimate.
+    #[expect(
+        clippy::result_large_err,
+        reason = "Boxing DryRunError would break backwards compatibility"
+    )]
     pub async fn gas_budget(
         &self,
         tx_kind: &TransactionKind,
@@ -523,6 +527,10 @@ impl SuiClient {
     }
 
     /// Estimate a budget for the transaction by dry-running it.
+    #[expect(
+        clippy::result_large_err,
+        reason = "Boxing DryRunError would break backwards compatibility"
+    )]
     pub async fn gas_budget_with_options(
         &self,
         tx_kind: &TransactionKind,
